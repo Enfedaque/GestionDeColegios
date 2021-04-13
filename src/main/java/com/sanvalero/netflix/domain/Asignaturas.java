bbd04672bea6d2e01ae4dd6222e8b6b0144151f6
David <davenfedaque@gmail.com>
2021-04-13 10:17:01 +0100

    MatriculadosDAO

--- src/main/java/com/sanvalero/netflix/domain/Asignaturas.java
+++ src/main/java/com/sanvalero/netflix/domain/Asignaturas.java
@@ -12,15 +12,18 @@ package com.sanvalero.netflix.domain;
 public class Asignaturas {
     
     private String ID_Asignatura;
+    private int horas;
+    private String dificultad;
 
-
-    public Asignaturas(String ID_Asignatura) {
+    //CONSTRUCTOR
+    public Asignaturas(String ID_Asignatura, int horas, String dificultad) {
         this.ID_Asignatura = ID_Asignatura;
-  
+        this.horas = horas;
+        this.dificultad = dificultad;
     }
-    
 
 
+   //GETTER Y SETTER
     public String getID_Asignatura() {
         return ID_Asignatura;
     }
@@ -28,5 +31,23 @@ public class Asignaturas {
     public void setID_Asignatura(String ID_Asignatura) {
         this.ID_Asignatura = ID_Asignatura;
     }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+    
+    
     
 }
